8ae4d1e333133fab99947d26009f8922ee7a28e0
Anilananda Chakrabortty <anilananda.chakrabortty@siemensgamesa.com>
2021-07-08 23:14:50 +0530

    added settings and user controller

---
+++ EShop.Api/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using EShop.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace EShop.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUser()
+        {
+            List<User> users = new List<User>()
+            {
+                new User()
+                {
+                    Id=1,
+                    UserName="Anilananda"
+                },
+                new User()
+                {
+                    Id=2,
+                    UserName="Arun"
+                }
+            };
+            return Ok(users);
+        }
+    }
+}
---
+++ EShop.Data/EShopDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EShop.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Data
+{
+    public class EShopDbContext : DbContext
+    {
+        public EShopDbContext(DbContextOptions<EShopDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
